e5d08d6fb130fc3d8ef494e19358a3283405d04c
AlexBKA <98500722+AlexBKA@users.noreply.github.com>
2025-04-11 16:53:14 -0600

    Add files via upload

---
+++ script_hw/helloworld.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Hello World
+echo.
+echo Listando el contenido del directorio actual:
+dir
+echo.
+mkdir Test
+cd Test
+echo.
+echo Listando el contenido del subdirectorio Test:
+dir
